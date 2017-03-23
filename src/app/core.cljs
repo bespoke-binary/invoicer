d932490b6817232ceaf49a95171dbea74035a303
Geoff Shannon <geoffpshannon@gmail.com>
2017-03-22 20:27:25 -0500

    Add parse-csv function using goog.labs.format.csv

--- src/app/core.cljs
+++ src/app/core.cljs
@@ -1,7 +1,11 @@
 (ns app.core
-  (:require [cljs.nodejs :as nodejs]))
+  (:require [cljs.nodejs :as nodejs])
+  (:import (goog.labs.format csv)))
 
 (nodejs/enable-util-print!)
+
+(defn parse-csv [s]
+  (js->clj (goog.labs.format.csv.parse s)))
 
 (defn main [& args]
   (println "Abracadabra!"))
